cedee04514bae9d4c09cd02cf46912be1d472cda
Dani <77969784+danielttttttt@users.noreply.github.com>
2025-09-26 13:43:45 +0300

    Fix Vercel deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:301',
-        changeOrigin: true,
-        secure: false
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    base: '/',
+    plugins: [react()],
+    define: {
+      'process.env': process.env
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:301',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
+    },
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      emptyOutDir: true,
+      sourcemap: true
     }
   }
 })
